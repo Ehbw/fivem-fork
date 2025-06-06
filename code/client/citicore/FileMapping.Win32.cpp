821af9c55b812c079c3bd46132d597a8870a50ed
Ehbw <ehbwwww@gmail.com>
2025-06-06 18:17:00 +0100

    tweak(citicore): block vulkan_dzn

--- code/client/citicore/FileMapping.Win32.cpp
+++ code/client/citicore/FileMapping.Win32.cpp
@@ -653,6 +653,8 @@ NTSTATUS NTAPI LdrLoadDllStub(const wchar_t* fileName, uint32_t* flags, UNICODE_
 #else
 		// VulkanRT loader, we don't use Vulkan, CEF does (to 'collect info'), and this crashes a lot of Vulkan drivers
 		(moduleNameStr.find(L"vulkan-1.dll") != std::string::npos && getenv("CitizenFX_ToolMode")) ||
+		// Microsoft Vulkan Compatability layers, crashes Vulkan Drivers
+		moduleNameStr.find(L"vulkan_dzn.dll") != std::string::npos ||
 #endif
 		// omen gaming hub (HP's software) - it crashes everyone that has this software installed for unknown reasons
 		moduleNameStr.find(L"omen_camera_x64.dll") != std::string::npos ||
