1f2f856f98ec176f685102fddb142ff87e2f9cc1
Ehbw <ehbwwww@gmail.com>
2026-05-20 16:04:44 +0100

    fix(nui/core): IME highlighting with Korean

--- code/components/nui-core/src/CefInput.cpp
+++ code/components/nui-core/src/CefInput.cpp
@@ -924,8 +924,7 @@ static HookFunction initFunction([] ()
 						// param is not used on Windows, so provide a default invalid value.
 						browser->GetHost()->ImeSetComposition(
 						cTextStr, underlines, CefRange(UINT32_MAX, UINT32_MAX),
-						CefRange(composition_start,
-						static_cast<int>(composition_start + cTextStr.length())));
+						CefRange(composition_start, composition_start));
 
 						// Update the Candidate Window position. The cursor is at the end so
 						// subtract 1. This is safe because IMM32 does not support non-zero-width
